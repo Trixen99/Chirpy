fb699d65b51ce4e1e935321f4c6fcfaabfdf5c6d
Trixen99 <nathanwhitehead99@hotmail.com>
2026-01-27 18:57:50 +0000

    added readiness endpoint

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import "net/http"
 
 func main() {
 	multiplexer := http.NewServeMux()
+
+	multiplexer.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
 	var server http.Server
 	server.Handler = multiplexer
 	server.Addr = ":8080"
